8b27fe661afc47d4b2b754ff5001fb7dbadbede1
Klngbob <529636014@qq.com>
2023-04-12 15:06:25 +0800

    day4 consistent hash done

---
+++ geecache/consistenthash/consistenthash.go
@@ -0,0 +1,57 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+// 映射bytes -> uint32
+type Hash func(key []byte) uint32
+
+type Map struct {
+	hash     Hash
+	replicas int            // 虚拟节点倍数
+	keys     []int          // 顺序哈希环
+	hashMap  map[int]string // 虚拟节点与真实节点的映射表
+}
+
+// 允许自定义虚拟节点倍数和hash函数
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+
+	return m
+}
+
+// 添加真实节点
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys)
+}
+
+// 获取给定key在hash环中的最近节点
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+	hash := int(m.hash([]byte(key)))
+	// 二分法查找
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
