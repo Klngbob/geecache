c00ebf21b98b2a699009636d9286680967e736cb
Klngbob <529636014@qq.com>
2023-04-12 21:46:20 +0800

    day5 perfectly done

--- run.sh
+++ run.sh
@@ -9,7 +9,14 @@ go build -o server
 sleep 2
 echo ">>> start test"
 curl "http://localhost:9999/api?key=KingBob" &
+curl "http://localhost:9999/api?key=Jane" &
+curl "http://localhost:9999/api?key=Nike" &
+sleep 2
 curl "http://localhost:9999/api?key=KingBob" &
+curl "http://localhost:9999/api?key=Jane" &
+curl "http://localhost:9999/api?key=Nike" &
+sleep 2
 curl "http://localhost:9999/api?key=KingBob" &
-
+curl "http://localhost:9999/api?key=Jane" &
+curl "http://localhost:9999/api?key=Nike" &
 wait
